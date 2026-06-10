9b802d57413819220cb783c0f44978f0e5805c32
yoseenajem <yosef.najem123@gmail.com>
2026-06-10 22:13:28 +0300

    fix(api): add ProductsModule to OrdersModule imports to resolve InventoryService dependency

--- apps/api/src/orders/orders.module.ts
+++ apps/api/src/orders/orders.module.ts
@@ -2,10 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { PaymentsModule } from '../payments/payments.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   // forwardRef: PaymentsModule imports OrdersModule too (circular: orders↔payments).
-  imports: [forwardRef(() => PaymentsModule)],
+  imports: [forwardRef(() => PaymentsModule), ProductsModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
